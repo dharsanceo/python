40e3aabce07e6dbcca9ccf9727440b0d4d87512d
dharsanceo <sdharsan13032014@gmail.com>
2025-02-27 21:16:38 +0530

    Add files via upload

---
+++ Draw_shapes/rec.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+
+turtle.forward(200)  
+turtle.left(90)      
+turtle.forward(200)  
+turtle.left(90)  
+turtle.forward(200)  
+turtle.left(90)  
+turtle.forward(200)  
+turtle.left(90)          
+
+turtle.done()
