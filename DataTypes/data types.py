c5377a846c5d8d961479d851f1efc4308e0432b6
dharsanceo <sdharsan13032014@gmail.com>
2025-02-27 21:11:25 +0530

    Add files via upload

---
+++ DataTypes/data types.py
@@ -0,0 +1,24 @@
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+city="Tirupur"
+print(city)
+print("city:", city)
+print("hi, Dharsan ! Welcome to Python world")
+print("name :", name)
+print("data types of name is", type(name))
+print("age :" , age)
+print("data types of age is", type(age))
+print("is_student :",is_student)
+print("data types of name is", type(is_student))
+print("weight :",weight)
+print("data types of name is", type(weight))
+
+print("\n After type cascading.....")
+age = str(age)
+print(age)
+print("data types of age is", type(age))
+weight =int(weight)
+print(weight)
+print("data types of name is", type(weight))
